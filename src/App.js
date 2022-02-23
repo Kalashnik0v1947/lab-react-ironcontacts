6a4b18fb2342127980c3460dd0865d9b1a54a271
Sergey Kadiyevskiy <51814805+Kalashnik0v1947@users.noreply.github.com>
2022-02-23 15:19:27 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,49 +1,107 @@
-import React from "react"
+import React from "react";
 import contacts from "./contacts.json";
-import './App.css';
-
-
-
+import "./App.css";
 
 function App() {
-  let fiveArr = contacts.splice(0,5)
-  // let remainArr = contacts.splice(5,contacts.length)
-  // console.log("this is the first 5 arr",fiveArr)
-  // console.log("this is the remainder arr",remainArr)
-  const [celebs, setCelebs] = React.useState(fiveArr) 
-  // console.log("this is 5 celebs",celebs)
-  
- 
+  let fiveArr = contacts.slice(0, 5);
+  let remainArr = contacts.slice(5, contacts.length);
+
+  const [celebs, setCelebs] = React.useState(fiveArr);
+  const [remainingCelebs, setRemainingCelebs] = React.useState(remainArr);
+  const [ascending, setAscending] = React.useState(true);
+
+  const addRandom = () => {
+    let randomCeleb =
+      remainingCelebs[Math.floor(Math.random() * remainingCelebs.length)];
+
+    if (!remainingCelebs.length) {
+      return;
+    }
+    setCelebs(celebs.concat(randomCeleb));
+
+    let newRemainCelebs = remainingCelebs.filter((celeb) => {
+      return celeb.name !== randomCeleb.name;
+    });
+
+    setRemainingCelebs(newRemainCelebs);
+  };
+
+  const orderAlph = () => {
+    let arrayCopy = [...celebs];
+    if (ascending) {
+      arrayCopy.sort((a, b) => (a.name > b.name ? 1 : -1));
+    } else {
+      arrayCopy.sort((a, b) => (a.name < b.name ? 1 : -1));
+    }
+
+    setCelebs(arrayCopy);
+    setAscending(!ascending);
+  };
+
+  const orderPop = () => {
+    let arrayCopy = [...celebs];
+    if (ascending) {
+      arrayCopy.sort((a, b) => a.popularity - b.popularity);
+    } else {
+      arrayCopy.sort((a, b) => b.popularity - a.popularity);
+    }
+
+    setCelebs(arrayCopy);
+    setAscending(!ascending);
+  };
+
+  const removeCeleb = (celebToRemove) => {
+    let fileteredArr = celebs.filter((celeb) => {
+      return celeb !== celebToRemove;
+    });
+    setCelebs(fileteredArr);
+  };
+
   return (
     <div className="App">
-      <table style={{border: "1px solid black"}}>
-      
-   
-   <tr >
-    <th>Picture</th>
-    <th>Name</th>
-    <th>Popularity</th>
-  </tr>
- 
-<td>
-    {celebs.map(function (celeb) {
-    return<tr> <img style={{width: "100px"}} src={celeb.pictureUrl}/></tr>
-    })}
-</td>
-<td>
-    {celebs.map(function (celeb) {
-      return<tr> <p style={{border: "1px solid black"}}>{celeb.name}</p></tr>
-    })}
-</td>
-<td>
-    {celebs.map(function (celeb) {
-    return<tr> <p style={{border: "1px solid black"}}>{celeb.popularity}</p></tr>
-    })}
-</td>
+      <b>Ironcontacts</b>
+      <button onClick={addRandom}>Add Random Contact</button>
+      <button onClick={orderAlph}>
+        Order By Name ({ascending ? "ascending" : "decending"})
+      </button>
+      <button onClick={orderPop}>
+        Sort By Popularity ({ascending ? "ascending" : "decending"})
+      </button>
+      <table style={{ border: "1px solid black" }}>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won an Oscar</th>
+          <th>Won an Emmy</th>
+          <th>Action</th>
+        </tr>
 
-
-  
-</table>
+        {celebs.map((props, i) => {
+          return (
+            <tr key={i}>
+              <td>
+                <img style={{ width: "100px" }} src={props.pictureUrl} />
+              </td>
+              <td>
+                <p>{props.name}</p>
+              </td>
+              <td>
+                <p>{Math.round(props.popularity * 100) / 100}</p>
+              </td>
+              <td>
+                <p>{props.wonOscar ? "🏆" : ""}</p>
+              </td>
+              <td>
+                <p>{props.wonEmmy ? "🏆" : ""}</p>
+              </td>
+              <td>
+                <button onClick={() => removeCeleb(props)}>destroy</button>
+              </td>
+            </tr>
+          );
+        })}
+      </table>
     </div>
   );
 }
