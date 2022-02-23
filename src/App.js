9ff1d8aa117454e4d66ef1d82f7fcc7b26516774
Sergey Kadiyevskiy <51814805+Kalashnik0v1947@users.noreply.github.com>
2022-02-22 21:56:26 -0500

    iteration 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React from "react"
+import contacts from "./contacts.json";
 import './App.css';
 
+
+
+
 function App() {
+  let fiveArr = contacts.splice(0,5)
+  // let remainArr = contacts.splice(5,contacts.length)
+  // console.log("this is the first 5 arr",fiveArr)
+  // console.log("this is the remainder arr",remainArr)
+  const [celebs, setCelebs] = React.useState(fiveArr) 
+  // console.log("this is 5 celebs",celebs)
+  
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table style={{border: "1px solid black"}}>
+      
+   
+   <tr >
+    <th>Picture</th>
+    <th>Name</th>
+    <th>Popularity</th>
+  </tr>
+ 
+<td>
+    {celebs.map(function (celeb) {
+    return<tr> <img style={{width: "100px"}} src={celeb.pictureUrl}/></tr>
+    })}
+</td>
+<td>
+    {celebs.map(function (celeb) {
+      return<tr> <p style={{border: "1px solid black"}}>{celeb.name}</p></tr>
+    })}
+</td>
+<td>
+    {celebs.map(function (celeb) {
+    return<tr> <p style={{border: "1px solid black"}}>{celeb.popularity}</p></tr>
+    })}
+</td>
+
+
+  
+</table>
     </div>
   );
 }
